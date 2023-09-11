5c9596c4694ca36790436b5583c002ae6c11e7ad
AdamHopeDuncan <101864052+AdamHopeDuncan@users.noreply.github.com>
2023-09-11 16:16:26 +0100

    Removed references to 'nuts' and changed criteria for finding location.

--- js/index.js
+++ js/index.js
@@ -17,8 +17,10 @@
                 return;
             }
 
+            console.log(data.result);
+
             // 2) Get the "nuts" data
-            const nuts = data.result.nuts;
+            const location = data.result.admin_county ? `${data.result.admin_county} County Council` : data.result.admin_district;
 
             let HTML = '<div style="margin-top:1.5rem;">We are sorry but the unitary authority or county council associated with your postcode could not be found.</div>'; // The output to display for the search results.
 
@@ -29,8 +31,10 @@
                 delimiter: ',',
                 complete: function (results) {
 
+                    console.log(location);
+
                     // 4) Match CSV data with "nuts" data as closely as possible
-                    const csvRow = results.data.find(row => row.nuts.trim().toLowerCase() === nuts.trim().toLowerCase());
+                    const csvRow = results.data.find(row => row.loc.trim().toLowerCase() === location.trim().toLowerCase());
 
                     if (csvRow) {
                         HTML = `<div style="margin-top:1.5rem;"><h2>${csvRow.name}</h2><p>${csvRow.description}</p></div>`;
