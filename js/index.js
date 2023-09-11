db9d6045b98a8067f6255912d2d9cde4e2157f5b
AdamHopeDuncan <101864052+AdamHopeDuncan@users.noreply.github.com>
2023-09-11 13:07:24 +0100

    Added code to get the postcode data from postcodes.io

--- js/index.js
+++ js/index.js
@@ -2,9 +2,24 @@
 
     window.addEventListener("load", function () {
 
-        document.querySelector("form").addEventListener("submit", function (evt) {
+        document.querySelector("form").addEventListener("submit", async function (evt) {
 
             evt.preventDefault();
+
+            // 1) Fetch from postcodes.io
+            const postcode = "B3 1UR"
+            const endPoint = `https://api.postcodes.io/postcodes/${postcode}`;
+            const response = await fetch(endPoint);
+            const data = await response.json();
+
+            // 2) Get the "nuts" data
+            const nuts = data.result.nuts;
+
+            // 3) Fetch CSV data
+
+            // 4) Match CSV data with "nuts" data as closely as possible
+
+            // 5) Pull the CSV data for the location
 
         });
 
