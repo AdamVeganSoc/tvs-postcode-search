06f5a4395b4a0a280e7e8459ef83b44e1ca1efca
AdamHopeDuncan <101864052+AdamHopeDuncan@users.noreply.github.com>
2023-09-11 14:51:56 +0100

    Added initial search result output, and output for invalid or not found results.

--- js/index.js
+++ js/index.js
@@ -12,22 +12,32 @@
             const response = await fetch(endPoint);
             const data = await response.json();
 
+            if (data.status === 404) {
+                document.querySelector('#search-results').innerHTML = `<div style="margin-top:1.5rem;">Invalid postcode</div>`;
+                return;
+            }
+
             // 2) Get the "nuts" data
             const nuts = data.result.nuts;
+
+            let HTML = '<div style="margin-top:1.5rem;">We are sorry but the unitary authority or county council associated with your postcode could not be found.</div>'; // The output to display for the search results.
 
             // 3) Fetch CSV data
             Papa.parse("./data/data.csv", {
                 download: true,
                 header: true,
                 delimiter: ',',
-                complete: function (results, file) {
-                    
-                    console.log(data.result, nuts.trim().toLowerCase());
+                complete: function (results) {
 
                     // 4) Match CSV data with "nuts" data as closely as possible
                     const csvRow = results.data.find(row => row.nuts.trim().toLowerCase() === nuts.trim().toLowerCase());
 
-                    console.log("Search result:", csvRow);
+                    if (csvRow) {
+                        HTML = `<div style="margin-top:1.5rem;"><h2>${csvRow.name}</h2><p>${csvRow.description}</p></div>`;
+                    }
+
+                    // Output the HTML result
+                    document.querySelector('#search-results').innerHTML = HTML;
 
                 }
             });
